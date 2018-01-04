5ce6b79366df23afca5e9c5d818f9e57638cc7f4
MyNEOWallet <neobetterthanqtum@example.com>
2018-01-03 21:30:57 -0800

    Adds myneowallet-browserify package

---
+++ myneowallet-browserify/compile.sh
@@ -0,0 +1,1 @@
+browserify main.js --s module > browserified.js
---
+++ myneowallet-browserify/main.js
@@ -0,0 +1,14 @@
+var BigInteger = require('bigi');
+var ec = require('elliptic').ec;
+var CryptoJS = require('crypto-js');
+var ecurve = require('ecurve');
+var Buffer = require('buffer');
+var baseX = require('base-x');
+module.exports = {
+  BigInteger,
+  CryptoJS,
+  ec,
+  ecurve,
+  Buffer,
+  baseX
+}
